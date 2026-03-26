f637afc16576861e17fb27fee752453c383057f9
myrnavance257-code <myrnavance257@gmail.com>
2026-03-26 15:03:58 +0800

    fixed

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,17 +1,16 @@
-package name.modid;
+package com.example.mixin;
 
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-public class VenszuUtility implements ModInitializer {
-    // This is the Logger, it lets you see mod messages in the Pojav/Zalith console
-	    public static final Logger LOGGER = LoggerFactory.getLogger("venszu-utility");
-
-		    @Override
-			    public void onInitialize() {
-				        // This code runs as soon as Minecraft starts loading
-						        LOGGER.info("Venszu Utility Main Initialized!");
-								    }
-									}
-								 
+@Mixin(ClientPlayerEntity.class)
+public class ExampleMixin {
+    @Inject(at = @At("HEAD"), method = "tick")
+        private void onTick(CallbackInfo info) {
+                // This is safe!
+                    }
+                    }
+                    
